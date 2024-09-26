69a6b4a8cb60ccb2d291b033fa79a97cee411178
SungJun1004 <121426998+SungJun1004@users.noreply.github.com>
2024-09-27 00:34:32 +0900

    Update Cal_Main.js

--- Cal_Main.js
+++ Cal_Main.js
@@ -57,4 +57,29 @@ const makeCalendar = (date) => {
 }
 const date = new Date();
 
-makeCalendar(date);
+
+
+(function () {
+  const template = document.createElement('template')
+  template.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        Hello Custom Widget
+        </div>
+      `
+  class Main extends HTMLElement {
+    constructor () {
+      super()
+
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
+
+      this._root = this._shadowRoot.getElementById('root')
+      makeCalendar(date);
+    }
+  }
+
+  customElements.define('com-sapkorea-sac-sungjun-cal01', Main)
+})()
+
