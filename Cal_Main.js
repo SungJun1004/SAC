a1ad34422a75b85bc6ea8ea8e85086fa392155f1
SungJun1004 <121426998+SungJun1004@users.noreply.github.com>
2024-10-02 01:06:52 +0900

    Update Cal_Main.js

--- Cal_Main.js
+++ Cal_Main.js
@@ -1,4 +1,5 @@
 (function () {
+
   // 임시 데이터
   const data = [
     { date: '2024-09-03', content: '테스트1' },
@@ -6,117 +7,155 @@
     { date: '2024-09-15', content: '테스트3' },
     { date: '2024-09-26', content: '테스트4' },
     { date: '2024-09-21', content: '테스트5' },
-  ]; 
-  
-  // 데이터 가공 함수
-  const processData = (data) => {
-    return data.reduce((acc, v) => {
-      acc[v.date] = [...(acc[v.date] || []), v.content];
-      return acc;
-    }, {});
-  };
-   // pad 메소드
-  const pad = (num) => (num > 9 ? num : '0' + num);
+  ];
 
-  const makeCalendar = function(date) {
+  // 데이터 가공
+  const calendarList = data.reduce(
+    (acc, v) => 
+      ({ ...acc, [v.date]: [...(acc[v.date] || []), v.content] })
+    , {}
+  );
+  // pad method 숫자 2자리로 통일
+  Number.prototype.pad = function() {
+    return this > 9 ? this : '0' + this;
+  }
+
+    const makeCalendar = function(date) {
     const currentYear = date.getFullYear();
     const currentMonth = date.getMonth() + 1;
 
     const firstDay = new Date(date.setDate(1)).getDay();
     const lastDay = new Date(currentYear, currentMonth, 0).getDate();
+
     const limitDay = firstDay + lastDay;
     const nextDay = Math.ceil(limitDay / 7) * 7;
 
     let htmlDummy = '';
 
-    // 전월 처리
+    //전월 처리
     for (let i = 0; i < firstDay; i++) {
-      htmlDummy += <div class="noColor"></div>;
+      htmlDummy += `<div class="noColor"></div>`;
     }
-
-    // 당월 처리
-    const calendarList = processData(data); // 데이터 가공
+   
+    // 당월처리
     for (let i = 1; i <= lastDay; i++) {
-      const dateStr = ${currentYear}-${pad(currentMonth)}-${pad(i)};
-      htmlDummy += 
-        <div class="currentColor">
+      const dateStr = `${currentYear}-${currentMonth.pad()}-${i.pad()}`;
+      htmlDummy += `
+        <div>
           ${i}
-          <br><br><br><br>
-          <p>${calendarList[dateStr]?.join('</p><p>') || ''}</p>
+           <br><br><br><br>
+           <p>${calendarList[dateStr]?.join('</p><p>') || ''}</p>
+         
         </div>
-      ;
+      `;
     }
-
-    // 이후 처리
+  
+    //  이후 처리
     for (let i = limitDay; i < nextDay; i++) {
-      htmlDummy += <div class="noColor"></div>;
+      htmlDummy += `<div class="noColor"></div>`;
     }
 
     this._dateBoard = this._shadowRoot.querySelector('.dateBoard');
     this._dateBoard.innerHTML = htmlDummy;
+    
   };
 
   const template = document.createElement('template');
-  template.innerHTML = 
-    <div class='rap'>
-      <div class="header">
-        <div id="dateTitle"></div>
-      </div>
-      <div class="grid dateHead">
-        <div>일</div>
-        <div>월</div>
-        <div>화</div>
-        <div>수</div>
-        <div>목</div>
-        <div>금</div>
-        <div>토</div>
-      </div>
-      <div class="grid dateBoard"></div>
+  template.innerHTML = `
+  <style>
+    .rap {
+      max-width: 1920px;
+      padding: 0 1.4rem;
+      margin-top: .3rem;
+    }
+    .dateHead {
+      margin-bottom: .4rem;
+    }
+    .dateHead div {
+      background: #B0E0E6;
+      color: #000;
+      text-align: center;
+    }
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(7, 1fr);
+      grid-gap: 5px;
+    }
+    .grid div {
+      padding: .6rem;
+      font-size: 1.1rem;
+    }
+    .dateBoard div {
+      color: #222;
+      font-weight: bold;
+      min-height: 8rem;
+      padding: .6rem .8rem;
+      border-radius: .6rem;
+      border: 1px solid #666;
+      text-align: right;
+    }
+    .noColor {
+      background: #eee;
+    }
+    .header {
+      display: flex;
+      justify-content: space-between;
+      padding: 1rem 2rem;
+    }
+    .dateBoard div p {
+      font-weight: normal;
+      margin-top: .2rem;
+    }
+   
+    * {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      box-sizing: border-box;  
+      font-family: Pretendard;
+    }
+  </style>
+
+  <div class='rap'>
+    <div class="header">
+      <div id="dateTitle"></div>
     </div>
-  ;
+    <div class="grid dateHead">
+      <div>일</div>
+      <div>월</div>
+      <div>화</div>
+      <div>수</div>
+      <div>목</div>
+      <div>금</div>
+      <div>토</div>
+    </div>
+    <div class="grid dateBoard"></div>
+  </div>
+  `;
 
   class Main extends HTMLElement {
     constructor() {
       super();
       this._shadowRoot = this.attachShadow({ mode: 'open' });
-      
-      // CSS 파일 로드
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.href = 'https://sungjun1004.github.io/SAC/style.css';
-      this._shadowRoot.appendChild(link);
-      
       this._shadowRoot.appendChild(template.content.cloneNode(true));
-      this.date = new Date(this.getAttribute('date') || '2024-09-10');
-      this.render();
+
+      const date = new Date('2024-09-10');
+      makeCalendar.call(this, date); 
     }
 
-    static get observedAttributes() {
-      return ['date'];
+   onCustomWidgetResize (width, height) {
+      this.render()
     }
 
-    attributeChangedCallback(name, oldValue, newValue) {
-      if (name === 'date') {
-        this.date = new Date(newValue);
-        this.render();
-      }
+    onCustomWidgetAfterUpdate (changedProps) {
     }
 
-    render() {
-         makeCalendar.call(this, this.date);
+    onCustomWidgetDestroy () {
     }
 
-    onCustomWidgetResize(width, height) {
-      this.render();
+    render () {
     }
-
-    onCustomWidgetAfterUpdate(changedProps) {
-      // 변경된 속성에 따라 추가 로직
-    }
-
-    onCustomWidgetDestroy() {
-      // 정리 로직
-    }
+    
   }
 
   customElements.define('com-sapkorea-sac-sungjun-cal01', Main);
