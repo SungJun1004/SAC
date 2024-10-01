792bb09fb2dd29a17e36017d627ad69c54589e0a
SungJun1004 <121426998+SungJun1004@users.noreply.github.com>
2024-10-02 00:04:57 +0900

    Update Cal_Main.js

--- Cal_Main.js
+++ Cal_Main.js
@@ -6,7 +6,7 @@
     { date: '2024-09-15', content: '테스트3' },
     { date: '2024-09-26', content: '테스트4' },
     { date: '2024-09-21', content: '테스트5' },
-  ];
+  ]; 
 
   // 데이터 가공 함수
   const processData = (data) => {
