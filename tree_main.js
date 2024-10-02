ea404662b0c822b982e2c1913eec591da58d2e90
SungJun1004 <121426998+SungJun1004@users.noreply.github.com>
2024-10-02 23:51:50 +0900

    Update and rename tree_Main.js to tree_main.js

--- tree_main.js
+++ tree_main.js
@@ -90,5 +90,5 @@ var parseMetadata = metadata => {
         }
     }
 
-    customElements.define('com-sap-sac-exercise-username-main', Main);
+    customElements.define('com-sapkorea-sac-sungjun-tree01', Main);
 })();
