b0193cef657b9f80e900ed5e90d3a4b80e0194c5
SungJun1004 <121426998+SungJun1004@users.noreply.github.com>
2024-10-07 23:54:44 +0900

    Update tree_main.js

--- tree_main.js
+++ tree_main.js
@@ -117,8 +117,8 @@ class Main extends HTMLElement {
     const averageRowHeight = 30; // 평균 높이
     const maxDepth = this._getMaxDepth(data); // 트리의 최대 깊이를 가져옵니다.
 
-   // const maxHeight = Math.min(rowCount * averageRowHeight, this.height) -100 ; // 최대 높이 설정
-    const maxHeight =  this.height - 10;
+    const maxHeight = Math.min(rowCount * averageRowHeight, this.height) ; // 최대 높이 설정
+    //const maxHeight =  this.height - 10;
     this._treeContainer.style.maxHeight = `${maxHeight}px`;
     }
 
